41a41ba5059fd2055b498bd2726e193d5fa2ea28
rachelchxn <85042944+rachelchxn@users.noreply.github.com>
2022-11-26 12:30:50 -0500

    finished cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,10 @@
-/*==================== SHOW MENU ====================*/
-// const showMenu = (toggleId, navId) =>{
-//     const toggle = document.getElementById(toggleId),
-//     nav = document.getElementById(navId)
-    
-//     // Validate that variables exist
-//     if(toggle && nav){
-//         toggle.addEventListener('click', ()=>{
-//             // We add the show-menu class to the div tag with the nav__menu class
-//             nav.classList.toggle('show-menu')
-//         })
-//     }
-// }
-// showMenu('nav-toggle','nav-menu')
-
-
-
 function toggleMode(){
     if(document.body.classList.contains("dark-theme")) {
         document.body.classList.remove("dark-theme");
+        document.querySelector('#toggle-mode').innerText = 'Dark Mode'
     } else {
-        document.body.classList.add("dark-theme");        
+        document.body.classList.add("dark-theme");    
+        document.querySelector('#toggle-mode').innerText = 'Light Mode'
     }
 
   }
@@ -30,35 +15,113 @@ let list = {
     spinach: 0
 }
 
+
 function addBbq() {
     list.bbq += 1
     console.log(list)
+    const menuBbq = document.querySelector('#menu-bbq')
+    menuBbq.innerText = `${list.bbq} Barbeque Salad ($22.00)`
+    menuBbq.classList.remove("hide-item");
+    const price = list.bbq*22 + list.salad*12 + list.spinach*9.5
+    const cartPrice = document.querySelector('#cart-price')
+    cartPrice.innerText = `Total: ${price.toFixed(2)}`
 }
 function addSalad() {
     list.salad += 1
     console.log(list)
+    const menuSalad = document.querySelector('#menu-salad')
+    menuSalad.innerText = `${list.salad} Salad with fish ($12.00)`
+    menuSalad.classList.remove("hide-item");
+    const price = list.bbq*22 + list.salad*12 + list.spinach*9.5
+    const cartPrice = document.querySelector('#cart-price')
+    cartPrice.innerText = `Total: ${price.toFixed(2)}`
 }
 function addSpinach() {
     list.spinach += 1
     console.log(list)
+    const menuSpinach = document.querySelector('#menu-spinach')
+    menuSpinach.innerText = `${list.spinach} Spinach salad ($9.50)`
+    menuSpinach.classList.remove("hide-item");
+    const price = list.bbq*22 + list.salad*12 + list.spinach*9.5
+    const cartPrice = document.querySelector('#cart-price')
+    cartPrice.innerText = `Total: ${price.toFixed(2)}`
 }
-
 
 function removeBbq() {
     if (list.bbq > 0) {
         list.bbq -= 1
         console.log(list)
+        const menuBbq = document.querySelector('#menu-bbq')
+        if (list.bbq === 0) {
+            menuBbq.innerText = ''
+            menuBbq.classList.add("hide-item");
+        } else {
+            menuBbq.innerText = `${list.bbq} Barbeque Salad ($22.00)`
+        }
+        const price = list.bbq*22 + list.salad*12 + list.spinach*9.5
+        const cartPrice = document.querySelector('#cart-price')
+        cartPrice.innerText = `Total: ${price.toFixed(2)}`
+        if (price === 0) {
+            cartPrice.innerText = 'Your cart is empty.'
+        }
     }
 }
 function removeSalad() {
     if (list.salad > 0) {
         list.salad -= 1
         console.log(list)
+        const menuSalad = document.querySelector('#menu-salad')
+        if (list.salad === 0) {
+            menuSalad.classList.add("hide-item");
+        } else {
+            menuSalad.innerText = `${list.salad} Salad with fish ($12.00)`
+        }
+        const price = list.bbq*22 + list.salad*12 + list.spinach*9.5
+        const cartPrice = document.querySelector('#cart-price')
+        cartPrice.innerText = `Total: ${price.toFixed(2)}`
+        if (price === 0) {
+            cartPrice.innerText = 'Your cart is empty.'
+        }
     }
 }
 function removeSpinach() {
     if (list.spinach > 0) {
         list.spinach -= 1
         console.log(list)
+        const menuSpinach = document.querySelector('#menu-spinach')
+        if (list.spinach === 0) {
+            menuSpinach.classList.add("hide-item");
+        } else {
+        menuSpinach.innerText = `${list.spinach} Spinach salad ($9.50)`
+        }
+        const price = list.bbq*22 + list.salad*12 + list.spinach*9.5
+        const cartPrice = document.querySelector('#cart-price')
+        cartPrice.innerText = `Total: ${price.toFixed(2)}`
+        if (price === 0) {
+            cartPrice.innerText = 'Your cart is empty.'
+        }
     }
+}
+
+function showCart() {
+    document.querySelector('#cart').classList.remove('hide-item')
+}
+
+function hideCart() {
+    document.querySelector('#cart').classList.add('hide-item')
+}
+
+function clearCart() {
+    const menuBbq = document.querySelector('#menu-bbq')
+    const menuSalad = document.querySelector('#menu-salad')
+    const menuSpinach = document.querySelector('#menu-spinach')
+    menuBbq.classList.add("hide-item");
+    menuSalad.classList.add("hide-item");
+    menuSpinach.classList.add("hide-item");
+    list.bbq = 0
+    list.salad = 0
+    list.spinach = 0
+    console.log(list)
+    const cartPrice = document.querySelector('#cart-price')
+    cartPrice.innerText = 'Your cart is empty.'
 }

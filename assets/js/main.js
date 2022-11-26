73d801942903a8dba894fbd50ae6cc74a20bd304
rachelchxn <85042944+rachelchxn@users.noreply.github.com>
2022-11-26 11:00:03 -0500

    working on js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,64 @@
 /*==================== SHOW MENU ====================*/
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+// const showMenu = (toggleId, navId) =>{
+//     const toggle = document.getElementById(toggleId),
+//     nav = document.getElementById(navId)
     
-    // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
-            nav.classList.toggle('show-menu')
-        })
+//     // Validate that variables exist
+//     if(toggle && nav){
+//         toggle.addEventListener('click', ()=>{
+//             // We add the show-menu class to the div tag with the nav__menu class
+//             nav.classList.toggle('show-menu')
+//         })
+//     }
+// }
+// showMenu('nav-toggle','nav-menu')
+
+
+
+function toggleMode(){
+    if(document.body.classList.contains("dark-theme")) {
+        document.body.classList.remove("dark-theme");
+    } else {
+        document.body.classList.add("dark-theme");        
+    }
+
+  }
+
+let list = {
+    bbq: 0,
+    salad: 0,
+    spinach: 0
+}
+
+function addBbq() {
+    list.bbq += 1
+    console.log(list)
+}
+function addSalad() {
+    list.salad += 1
+    console.log(list)
+}
+function addSpinach() {
+    list.spinach += 1
+    console.log(list)
+}
+
+
+function removeBbq() {
+    if (list.bbq > 0) {
+        list.bbq -= 1
+        console.log(list)
     }
 }
-showMenu('nav-toggle','nav-menu')
-
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.getElementById('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+function removeSalad() {
+    if (list.salad > 0) {
+        list.salad -= 1
+        console.log(list)
+    }
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-function scrollHeader(){
-    const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
-
-/*==================== SHOW SCROLL TOP ====================*/ 
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollTop)
-
-
-
-var modal;
-
-var btn;
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-
+function removeSpinach() {
+    if (list.spinach > 0) {
+        list.spinach -= 1
+        console.log(list)
+    }
 }
